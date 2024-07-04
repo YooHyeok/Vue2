f6ad284d7579e0c6e4becad1bbbbf0a931dd19cf
webdevyoo <yjk7454@naver.com>
2024-07-04 23:53:34 +0900

    feat: `App.vue` - template와 script 영역 구분 및 data함수 모듈 추출 및 상호작용 기초 샘플 코드 적용

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,19 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ count }}</p>
+    <button v-on:click="count ++">추가</button>
+    <button @click="count --">감소</button>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  data () {
+    return {
+      title: "안녕하세요.",
+      count: 1
+    }
+  }
+}
 </script>
