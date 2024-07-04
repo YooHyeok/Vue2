956dff3826b81da110c2cc0cf9ce2dced809b3b6
webdevyoo <yjk7454@naver.com>
2024-07-05 00:20:46 +0900

    feat: 컴포넌트 생성 및 사용 - 단일 컴포넌트 파일 간 import

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,18 @@
     <p>{{ count }}</p>
     <button v-on:click="count ++">추가</button>
     <button @click="count --">감소</button>
+    <Home/>
   </div>
 </template>
 
 <script>
+
+import Home from "./Home.vue"
+
 export default {
+  components : {
+    Home
+  },
   data () {
     return {
       title: "안녕하세요.",
