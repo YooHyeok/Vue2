b048cb117497bf9e22de0a3ac543bccb03fb41a8
webdevyoo <yjk7454@naver.com>
2024-07-06 00:24:58 +0900

    feat: basic, user 컴포넌트 router 등록

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import User from '../components/user/User.vue'
+import Basic from '../components/Basic.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/user',
+    name: 'user',
+    component: User
+  },
+  {
+    path: '/basic',
+    name: 'basic',
+    component: Basic
   },
   {
     path: '/about',
