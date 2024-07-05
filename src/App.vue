2b7e7441310ca3b5bc8cd67b71b257a446edd966
webdevyoo <yjk7454@naver.com>
2024-07-06 00:23:30 +0900

    refactor: Vue 튜토리얼, 컴포넌트 Basic.vue 컴포넌트 추출

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <p>{{ count }}</p>
-    <button v-on:click="count ++">추가</button>
-    <button @click="count --">감소</button>
-    <Home/>
-    <Appstatus/>
-  </div>
+  <v-app>
+    <v-content>
+      <router-view></router-view>	
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 
-import Home from "./Home.vue"
-
 export default {
-  components : {
-    Home
-  },
-  data () {
-    return {
-      title: "안녕하세요.",
-      count: 1
-    }
-  }
+
 }
 </script>
---
+++ src/components/Basic.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ count }}</p>
+    <button v-on:click="count ++">추가</button>
+    <button @click="count --">감소</button>
+    <Home/>
+    <Appstatus/>
+  </div>
+  
+</template>
+
+<script>
+
+import User from "../components/user/User.vue";
+import Home from "../Home.vue"
+
+export default {
+  components : {
+    Home,
+    User
+  },
+  data () {
+    return {
+      title: "안녕하세요.",
+      count: 1
+    }
+  }
+}
+</script>
