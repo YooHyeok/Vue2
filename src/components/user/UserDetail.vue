6c76901d8cd7c3af30bcb2f3d7e21755a743ff30
webdevyoo <yjk7454@naver.com>
2024-07-07 00:26:15 +0900

    faet: 부모 컴포넌트로 부터 받은 props를 수정하는 메소드 추가

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -3,6 +3,8 @@
     <h3>자세한 회원 정보를 확인합니다.</h3>
     <p>{{ nameOfChild }}</p>
     <p>{{ sayHello }}</p>
+    <v-btn @click="switchName()">이름 변경</v-btn>
+
   </div>
 </template>
 
@@ -19,6 +21,11 @@ export default {
   computed: { // 자식 컴포넌트에서 props값 활용하기.
     sayHello () {
       return this.nameOfChild + `하이!`
+    }
+  },
+  methods: {
+    switchName () { // 부모 컴포넌트의 props 값 수정.
+      this.nameOfChild = '컴퓨터'
     }
   },
   data () {
