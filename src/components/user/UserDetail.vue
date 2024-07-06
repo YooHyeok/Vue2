e5922d12ac06655aed2a826310d3dc70b6a5173f
webdevyoo <yjk7454@naver.com>
2024-07-06 23:51:44 +0900

    feat: props 타입 및 세부옵션 지정

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -2,12 +2,20 @@
   <div class="red lighten-3 pa-3">
     <h3>자세한 회원 정보를 확인합니다.</h3>
     <p>{{ nameOfChild }}</p>
+    <p>{{ sayHello }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['nameOfChild'],
+  // props: ['nameOfChild'],
+  props: {
+    // nameOfChild: String
+    nameOfChild: {
+      type: String,
+      required: true // 필수 값 여부
+    }
+  },
   data () {
     return {
     }
