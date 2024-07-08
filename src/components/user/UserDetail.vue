09c19f392e1b4e1b7acc7b2d4a23c4a5c033ab50
webdevyoo <yjk7454@naver.com>
2024-07-08 21:04:48 +0900

    feat: 회원 정보 상세 컴포넌트 template 코드 구성 및 부모 props 등록

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -1,26 +1,55 @@
 <template>
-  <div class="red lighten-3 pa-3">
+  <!-- <div class="red lighten-3 pa-3">
     <h3>자세한 회원 정보를 확인합니다.</h3>
     <p>{{ nameOfChild }}</p>
     <p>{{ sayHello }}</p>
     <v-btn @click="switchName()">이름 변경</v-btn>
+  </div> -->
+  <div class="red lighten-3 pa-3">
+    <h3>자세한 회원 정보를 확인합니다.</h3>
+    <p>상세사항</p>
+<!--     <p>{{ nameOfChild }}</p>
+    <p>{{ sayHello }}</p>
+    <v-btn @click="switchName()">이름 변경</v-btn> -->
+    <v-list >
+      <v-list-item>
+        <v-list-item-content>이름 : </v-list-item-content>
+        <v-list-item-content class="d-inline text-end">{{ name }}</v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>주소 : </v-list-item-content>
+        <v-list-item-content class="d-inline text-right">{{ address }}</v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>전화번호 : </v-list-item-content>
+        <v-list-item-content class="d-inline text-end">{{ phone }}</v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>반려견유무 : </v-list-item-content>
+        <v-list-item-content class="d-inline text-right">{{ hasDogKr }}</v-list-item-content>
+      </v-list-item>
 
+    </v-list>
   </div>
 </template>
 
 <script>
+
 export default {
-  // props: ['nameOfChild'],
-  props: {
+  props: ['nameOfChild', 'name', 'address', 'phone', 'hasDog'],
+ /*  props: {
     // nameOfChild: String
     nameOfChild: {
       type: String,
       required: true // 필수 값 여부
-    }
-  },
+    },
+  }, */
   computed: { // 자식 컴포넌트에서 props값 활용하기.
     sayHello () {
       return this.nameOfChild + `하이!`
+    },
+    hasDogKr() {
+      return this.hasDog === true ? '있음' : '없음'
     }
   },
   methods: {
@@ -33,4 +62,5 @@ export default {
     }
   } 
 }
+
 </script>
